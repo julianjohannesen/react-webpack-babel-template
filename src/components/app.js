ac4dce3b9a8df8f39e896609293cf61f03502ebd
julianjohannesen <github email address>
2019-02-14 13:16:48 -0500

    Done

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => {
+    return (
+        <div className="full-screen">
+            <h1>
+                React Page
+            </h1>
+        </div>
+    );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,47 @@
-const webpack = require('webpack');
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-//Trying it w/o const config = and separate module.export at bottom
 module.exports = {
-    entry: './src/app.js',
+    entry: path.join(__dirname, "src", "index.js"),
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.join(__dirname, "build"),
+        filename: "bundle.js"
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /.(css|scss)$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[path][name]-[hash:8].[ext]"
+                        }
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.join(__dirname, "src", "index.html")
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 };
